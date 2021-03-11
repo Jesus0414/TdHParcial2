c483254354188cbf1aa844d1444c83bfb0a9e69c
Jesus_0414 <jesus140400@gmail.com>
2021-03-11 15:46:48 -0700

    Bytes a enteros

--- Practica1.py
+++ Practica1.py
@@ -1,7 +1,16 @@
 import wave
+import numpy as np
 
+#Cargar  archivo wav en la variable 
 GM = wave.open('good-morningMan.wav', 'r')
 
 frames = GM.readframes(-1)
 
-print(frames[:100])
+#mostrar el resultado frames
+#print(frames[:10])
+
+#Convierte el audio good morning de byte a enteros
+ondaconvertida = np.frombuffer(frames, dtype='int16')
+
+#Muestra los primeros 10 int
+print(ondaconvertida[:10])
